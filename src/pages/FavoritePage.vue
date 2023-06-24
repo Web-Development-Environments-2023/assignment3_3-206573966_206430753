9809785f609456f52781f6ff45d4804c54448a7d
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-06-25 01:12:39 +0300

    Update FavoritePage.vue

--- src/pages/FavoritePage.vue
+++ src/pages/FavoritePage.vue
@@ -1,12 +1,84 @@
 <template>
     <div>
-        <h1>"Favorite"</h1>
+    <b-container>
+      <h3>
+        {{this.titleFavorite}}
+        <slot></slot>
+      </h3>
+      <b-col>
+        <b-row v-for="r in recipes" :key="r.id">
+          <RecipePreview class="recipePreview" :recipe="r" />
+        </b-row>
+      </b-col>
+    </b-container>
     </div>
-</template>
-
-<script>
-
-</script>
-
-<style>
-</style>
+  </template>
+  
+  <script>
+  import RecipePreview from "../components/RecipePreview.vue";
+  export default {
+    name: "RecipePreviewList",
+    components: {
+      RecipePreview
+    },
+    // props: {
+    //   title: {
+    //     type: String,
+    //     required: true
+    //   }
+    // },
+    data() {
+      return {
+        recipes: [],
+        titleFavorite:" "
+      };
+    },
+    mounted() {
+      this.updateRecipes();
+    },
+    methods: {
+      async updateRecipes() {
+        try {
+          const response = await this.axios.get(
+            // this.$root.store.server_domain + "/recipes/random",
+            this.$root.store.server_domain+"/users/favorites",
+            {withCredentials:true}
+          );
+  
+          console.log(response);
+          const recipes = response.data;
+          let temp=[]
+          recipes.forEach(element => {
+            if(element!=null)
+            {
+                temp.push(element)
+            }
+          });
+          if(temp.length>0)
+          {
+            this.titleFavorite="Favorite:"
+          }
+          else
+          {
+            this.titleFavorite="you dont have favorite yet"
+          }
+          this.recipes = [];
+          this.recipes.push(...temp);
+          // console.log(this.recipes);
+        } catch (error) {
+          console.log(error);
+        }
+      }
+  
+  
+      
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .container {
+    min-height: 400px;
+  }
+  </style>
+  
