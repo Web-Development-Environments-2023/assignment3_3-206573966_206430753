042d79b88274d25980d5ebe0de09905954e479b2
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-06-25 01:17:54 +0300

    Add files via upload

---
+++ src/components/MyRecipeList.vue
@@ -0,0 +1,67 @@
+<template>
+    <b-container>
+      <h3>
+        {{ this.title }}
+        <slot></slot>
+      </h3>
+      <b-col>
+        <b-row v-for="r in recipes" :key="r.id">
+          <MyRecipePreivew class="recipePreview" :recipe="r" />
+        </b-row>
+      </b-col>
+    </b-container>
+  </template>
+  
+  <script>
+  import MyRecipePreivew from "./MyRecipePreivew.vue";
+  export default {
+    name: "RecipePreviewList",
+    components: {
+        MyRecipePreivew
+    },
+    // props: {
+    //   title: {
+    //     type: String,
+    //     required: true
+    //   }
+    // },
+    data() {
+      return {
+        recipes: [],
+        title:""
+      };
+    },
+    mounted() {
+      this.updateRecipes();
+    },
+    methods: {
+      async updateRecipes() {
+        try {
+          const response = await this.axios.get(
+            "http://127.0.0.1:3000/users/returnMyRecipe",{withCredentials:true}
+          );
+            console.log(response.data)
+          const recipes = response.data;
+          this.recipes = [];
+          this.recipes.push(...recipes);
+          if(this.recipes.length==0)
+          {
+            this.title="You dont have recipe yet"
+          }
+        } catch (error) {
+          console.log(error);
+        }
+      }
+  
+  
+      
+    }
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .container {
+    min-height: 400px;
+  }
+  </style>
+  
