0d3f33aab5f97ddc8ade622b89f21cb1156bacb1
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-06-25 01:12:20 +0300

    Update FamilyRecipePage.vue

--- src/pages/FamilyRecipePage.vue
+++ src/pages/FamilyRecipePage.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        <h1>"Family"</h1>
+    <div class="container">
+      <h1 class="title">My Family Recipe</h1>
+      <b-row>
+      <b-col>
+      <MyFamilyRecipeList title="My Family Recipes" class="RandomRecipes center"/>
+      </b-col>
+    </b-row>
     </div>
-</template>
-
-<script>
-
-</script>
-
-<style>
-</style>
+  </template>
+  
+  <script>
+  import MyFamilyRecipeList from "../components/MyFamilyRecipeList.vue";
+  export default {
+    components: {
+        MyFamilyRecipeList
+    },
+  };
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  .RandomRecipes {
+    margin: 10px 0 10px;
+  }
+  .blur {
+    -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
+    filter: blur(2px);
+  }
+  ::v-deep .blur .recipe-preview {
+    pointer-events: none;
+    cursor: default;
+  }
+  </style>
+  
