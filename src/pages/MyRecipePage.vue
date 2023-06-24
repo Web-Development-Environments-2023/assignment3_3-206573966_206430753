daee61a1a0c013b6813b70876dfd8effb44b5c16
RonyShir24 <127685032+RonyShir24@users.noreply.github.com>
2023-06-25 01:13:33 +0300

    Update MyRecipePage.vue

--- src/pages/MyRecipePage.vue
+++ src/pages/MyRecipePage.vue
@@ -1,12 +1,35 @@
 <template>
-    <div>
-        <h1>"My recipe"</h1>
+    <div class="container">
+      <h1 class="title">My Recipe</h1>
+      <b-row>
+      <b-col>
+      <MyRecipeList title="My Recipes" class="RandomRecipes center"/>
+      </b-col>
+    </b-row>
     </div>
-</template>
-
-<script>
-
-</script>
-
-<style>
-</style>
+  </template>
+  
+  <script>
+  import MyRecipeList from "../components/MyRecipeList.vue";
+  export default {
+    components: {
+        MyRecipeList
+    },
+  };
+  
+  </script>
+  
+  <style lang="scss" scoped>
+  .RandomRecipes {
+    margin: 10px 0 10px;
+  }
+  .blur {
+    -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
+    filter: blur(2px);
+  }
+  ::v-deep .blur .recipe-preview {
+    pointer-events: none;
+    cursor: default;
+  }
+  </style>
+  
