172179b2a58b6fcbd177d8c38c47227890b81932
NetaKirmayer <103743241+NetaKirmayer@users.noreply.github.com>
2023-07-23 18:45:42 +0300

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,7 @@ module.exports = {
     devtool: "source-map"
   },
   devServer: {
-    // host: "132.72.65.211"
-    host: "127.0.0.1"
+    host: "localhost"
   }
   // publicPath: process.env.NODE_ENV === "production" ? "/LAB12/" : "/"
 };
